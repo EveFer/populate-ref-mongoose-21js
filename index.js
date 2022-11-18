5519afe6cf1a14985a9f3e3746c601c570400698
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-11-18 11:33:56 -0600

    populate

--- index.js
+++ index.js
@@ -9,31 +9,31 @@ const personSchema = Schema({
   name: String,
   age: Number
 });
-const commentSchema = Schema({
-    comment: {
-        type: String
-    },
-    author: { type: Schema.Types.ObjectId, ref: 'Person' }
-  });
+// const commentSchema = Schema({
+//     comment: {
+//         type: String
+//     },
+//     author: { type: Schema.Types.ObjectId, ref: 'Person' }
+//   });
 
 const storySchema = Schema({
   author: { type: Schema.Types.ObjectId, ref: 'Person' },
   title: String,
-  comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
+  // comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
 });
 
 const Story = mongoose.model('Story', storySchema);
 const Person = mongoose.model('Person', personSchema);
-const Comments = mongoose.model('Comment', commentSchema);
+// const Comments = mongoose.model('Comment', commentSchema);
 
 async function createPerson() {
-    await Person.create({name: 'Fer Toledo', age: 25})
+    await Person.create({name: 'Lore', age: 25})
 }
 
 function createPost() {
     const newPost = {
-        title: 'Como consumir mis endpoints desde una app movil',
-        author: '634782bd494d1e2fd6434912'
+        title: 'Como crear un proyecto con react',
+        author: '6350c8fc7dc601244d2c4963'
     }
 
     return Story.create(newPost)
@@ -49,7 +49,7 @@ mongoose.connect(URL)
         // const postCreated = await createPost()
         // console.log(postCreated)
 
-        const storyFound = await Story.findById('634782f371cf1d7b805106b6').populate('author comments')
+        const storyFound = await Story.findById('6350c94b4ede9b03dcf61d1b').populate('author')
         // const storyFound = await Story.findById('634776d5ed256b16948cbf0b').populate({path: 'author', select: ['name']})
         console.log(storyFound)
     })
